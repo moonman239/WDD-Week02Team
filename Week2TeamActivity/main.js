775ad2ea5c3c33676be9da1a35fba8062ab83921
Montana Burr <bur18019@byui.edu>
2021-05-01 22:21:26 -0600

    Added part #1 to HTML file and changed some ids.

--- Week2TeamActivity/main.js
+++ Week2TeamActivity/main.js
@@ -1,15 +1,15 @@
 function sumToInputtedNumber() {
-    let textbox = parseInt(document.getElementById("textbox").value);
+    let textbox = parseInt(document.getElementById("textbox_02").value);
     let output = 0;
     for (let i=1; i<=textbox; i+=1)
       output = output + i;
-    document.getElementById("contents").innerHTML = output;
+    document.getElementById("sum").innerHTML = output;
   }
   
 function sum() {
     alert("hi");
-    var input1 = parseInt(document.getElementById("textbox1").value);
-    var input2 = parseInt(document.getElementById("textbox2").value);
+    var input1 = parseInt(document.getElementById("textbox1_03").value);
+    var input2 = parseInt(document.getElementById("textbox2_03").value);
     alert(input1 + input2);
     document.getElementById("result").innerHTML = input1 + input2;
   }

3a64eb8dc3929b41b69cf0514d5a38ec03f092ae
Montana Burr <bur18019@byui.edu>
2021-05-01 22:28:16 -0600

    Got parts #1 and #3 working.

--- Week2TeamActivity/main.js
+++ Week2TeamActivity/main.js
@@ -1,3 +1,9 @@
+function display()
+{
+    let textbox = document.getElementById("textbox_01").value;
+    document.getElementById("contents").innerHTML = textbox;
+}
+
 function sumToInputtedNumber() {
     let textbox = parseInt(document.getElementById("textbox_02").value);
     let output = 0;
@@ -7,9 +13,7 @@ function sumToInputtedNumber() {
   }
   
 function sum() {
-    alert("hi");
     var input1 = parseInt(document.getElementById("textbox1_03").value);
     var input2 = parseInt(document.getElementById("textbox2_03").value);
-    alert(input1 + input2);
     document.getElementById("result").innerHTML = input1 + input2;
   }
